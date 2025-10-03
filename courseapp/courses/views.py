81a8824fc315ff590eceec87031a0a8c02b59c1f
Nida Ayhan <nida0ayhan@gmail.com>
2025-10-03 09:33:23 -0700

    local global files

--- courseapp/courses/views.py
+++ courseapp/courses/views.py
@@ -1,4 +1,4 @@
-from datetime import date
+from datetime import date, datetime
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.shortcuts import redirect, render
 from django.urls import reverse
@@ -16,16 +16,16 @@ db = {
         {
             "title": "javascript kursu",
             "description": "javascript kurs açıklaması",
-            "imageUrl": "https://img-c.udemycdn.com/course/750x422/1662526_fc1c_3.jpg",
+            "imageUrl": "1.webp",
             "slug": "javascript-kursu",
-            "date": date(2022,10,10),
+            "date": datetime.now,
             "isActive": True,
             "isUpdated": True
         },
         {
             "title": "python kursu",
             "description": "python kurs açıklaması",
-            "imageUrl": "https://img-c.udemycdn.com/course/750x422/2463492_8344_3.jpg",
+            "imageUrl": "2.webp",
             "slug": "python-kursu",
             "date": date(2022,10,10),
             "isActive": False,
@@ -35,7 +35,7 @@ db = {
         {
             "title": "web geliştirme kursu",
             "description": "web geliştirme kurs açıklaması",
-            "imageUrl": "https://img-c.udemycdn.com/course/750x422/1258436_2dc3_4.jpg",
+            "imageUrl": "3.webp",
             "slug": "web-gelistirme-kursu",
             "date": date(2022,10,10),
             "isActive": True,
