73ba32143611824a99b040f9504933d87b802cbc
Nida Ayhan <nida0ayhan@gmail.com>
2025-10-09 09:51:13 -0700

    admin panel

--- courseapp/courses/models.py
+++ courseapp/courses/models.py
@@ -1,17 +1,29 @@
 from django.db import models
+from django.utils.text import slugify
 
+
+class Category(models.Model):
+    name = models.CharField(max_length=40)
+    slug = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.name}"
+    
 # Create your models here.
 class Course(models.Model):
     title= models.CharField(max_length=50)
     description = models.TextField()
     imageUrl= models.CharField(max_length=50,blank=True)
-    date= models.DateField()
+    date= models.DateField(auto_now=True)
     isActive= models.BooleanField()
+    slug= models.SlugField(default="",blank=True, editable=False, null=False, unique=True, db_index=True)
+    category = models.ForeignKey(Category, default=1, on_delete=models.CASCADE)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(args,kwargs)
 
     def __str__(self):
         return f"{self.title} {self.date}"
     
 
-class Category(models.Model):
-    name = models.CharField(max_length=40)
-    slug = models.CharField(max_length=50)
